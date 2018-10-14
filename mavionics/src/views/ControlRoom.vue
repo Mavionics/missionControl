abe2702b3faf1f82e2ce829f0263cb652ea5f366
Henrik Wolkesson <henrik.wolkesson@gmail.com>
2018-10-14 16:43:21 +0200

    Cesium map view in controlroom

--- mavionics/src/views/ControlRoom.vue
+++ mavionics/src/views/ControlRoom.vue
@@ -4,10 +4,10 @@
     <section class="hero is-info is-fullheight">
       <div class="hero-head">
         <Navbar/>
-        
         <div class="box is-glass">
           <h1>Control Room for {{displayName}}.</h1>
           <div>You are in the Control Room! Mind your step!</div>
+          <div id="cesiumContainer" class="fullscreen"></div>
           <table class="table is-hoverable is-fullwidth">
             <thead>
               <tr>
@@ -22,8 +22,9 @@
                   v-bind:key="vehicle.id">
                 <th>{{vehicle.name}}</th>
                 <td>{{vehicle.id}}</td>
-                <td>-</td>
-                <td>-</td>
+
+                <td v-if="vehicle.position">{{vehicle.position.latitude}}, {{vehicle.position.longitude}} </td>
+                <td></td>
               </tr>
             </tbody>
           </table>
@@ -33,13 +34,49 @@
   </div>
 </template>
 
+<style scoped>
+.fullscreen {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
+
+
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Cesium from "cesium/Cesium";
+require("cesium/Widgets/widgets.css");
 
 export default {
   name: "ControlRoom",
   components: {
     Navbar
+  },
+  mounted() {
+    Cesium.Ion.defaultAccessToken =
+      "***REMOVED***";
+
+    this.viewer = new Cesium.Viewer("cesiumContainer", {
+      imageryProvider: Cesium.createWorldImagery(), // use the Bing Maps Aerial imagery from ion (this is the default)
+      terrainProvider: Cesium.createWorldTerrain(), // use the Cesium World Terrain from ion
+      timeline: false, // Hide widgets
+      infoBox: false,
+      animation: false,
+      fullscreenButton: false
+    });
+
+    // var pinBuilder = new Cesium.PinBuilder();
+
+    // this.viewer.entities.add({
+    //   position: Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+    //   billboard: {
+    //     image : pinBuilder.fromText('?', Cesium.Color.BLACK, 48).toDataURL(),
+    //     verticalOrigin : Cesium.VerticalOrigin.BOTTOM
+    //   },
+    // });
   },
   computed: {
     displayName() {
@@ -55,6 +92,45 @@ export default {
     return {
       name: ""
     };
+  },
+  watch: {
+    vehicles(val) {
+      val.forEach(vehicle => {
+        if (vehicle.position) {
+          console.log(
+            vehicle.position.longitude + " " + vehicle.position.latitude
+          );
+
+          var v = this.viewer.entities.getById(vehicle.name);
+          if (!v) {
+            console.log("New vehicle");
+
+            var pinBuilder = new Cesium.PinBuilder();
+            this.viewer.entities.add({
+              id: vehicle.name,
+              position: Cesium.Cartesian3.fromDegrees(
+                vehicle.position.longitude,
+                vehicle.position.latitude
+              ),
+              billboard: {
+                image: pinBuilder
+                  .fromText(vehicle.name, Cesium.Color.BLACK, 70)
+                  .toDataURL(),
+                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
+                disableDepthTestDistance: Number.POSITIVE_INFINITY
+              }
+            });
+          } else {
+            v.position = Cesium.Cartesian3.fromDegrees(
+              vehicle.position.longitude,
+              vehicle.position.latitude
+            );
+            console.log("Position updated " + v.position);
+          }
+        }
+      });
+    }
   }
 };
 </script>
--- mavionics/vue.config.js
+++ mavionics/vue.config.js
@@ -1,5 +1,44 @@
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
+
+const path = require('path');
+const CopywebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
-    configureWebpack: {
-      devtool: 'source-map'
-    }
+  configureWebpack: {
+    devtool: 'source-map',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+
+      // Needed to compile multiline strings in Cesium
+      sourcePrefix: ''
+    },
+    amd: {
+      // Enable webpack-friendly use of require in Cesium
+      toUrlUndefined: true
+    },
+    node: {
+      // Resolve node module use of fs 
+      // (Propably only needing in NodeJS and not in browser)
+      fs: 'empty'
+    },
+    resolve: {
+      alias: {
+        // Cesium module name
+        cesium: path.resolve(__dirname, cesiumSource)
+      }
+    },
+    plugins: [
+      // Copy Cesium Assets, Widgets, and Workers to a static directory
+      new CopywebpackPlugin([{ from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' }]),
+      new CopywebpackPlugin([{ from: path.join(cesiumSource, 'Assets'), to: 'Assets' }]),
+      new CopywebpackPlugin([{ from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }]),
+      new webpack.DefinePlugin({
+        // Define relative base path in cesium for loading assets
+        CESIUM_BASE_URL: JSON.stringify('../')
+      }),
+    ],
   }
+}
