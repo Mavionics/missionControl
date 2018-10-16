3a2e4511ff7ed220e20596ba01a58a679a788810
Henrik Wolkesson <henrik.wolkesson@gmail.com>
2018-10-16 13:07:17 +0200

    Styled About page as the other pages

--- mavionics/src/views/About.vue
+++ mavionics/src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="hero is-info is-fullheight">
+      <div class="hero-head">
+        <Navbar/>
+      </div>
+      <div class="hero-body has-text-centered">
+        <div class="container">
+          <h1>This is an about page</h1>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "about",
+  components: {
+    Navbar
+  }
+};
+</script>
