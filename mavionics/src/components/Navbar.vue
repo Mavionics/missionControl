fa9377b585d44196b80f85b7f497653880635b7b
Henrik Wolkesson <henrik.wolkesson@gmail.com>
2018-10-28 16:41:06 +0100

    Navbar indicated active page

--- mavionics/src/components/Navbar.vue
+++ mavionics/src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="content">
-        <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <router-link class="navbar-item" to="/">
-            <img id="navbar-brand-img" v-if="showBrand" src="../assets/logo.svg" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" >
+            <router-link class="navbar-item" v-if="showBrand" to="/">
+              <img id="navbar-brand-img" src="../assets/logo.svg" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
             </router-link>
 
             <a role="button" class="navbar-burger" @click="showHamburger" :class="{'is-active':navIsActive}" data-target="navMenu" aria-label="menu" aria-expanded="false">
@@ -13,12 +13,16 @@
             </a>
         </div>
         <div class="navbar-menu" :class="{'is-active':navIsActive}">
-        <div class="navbar-end">      
-            <router-link class="navbar-item" to="/">Home</router-link>
-            <router-link class="navbar-item" to="/controlroom">ControlRoom</router-link>
-            <router-link class="navbar-item" to="/about">About</router-link>
-            <router-link class="navbar-item" to="/logout" v-show="loggedIn" @click="logout"><div class="button">Logout</div></router-link>
         </div>
+        <div class="navbar-end">      
+            <router-link name="Home" class="navbar-item" to="/">Home</router-link>
+            <router-link name="ControlRoom" class="navbar-item" to="/controlroom">ControlRoom</router-link>
+            <router-link name="About" class="navbar-item" to="/about">About</router-link>
+            <div class="navbar-item">
+              <div class="buttons">
+                <router-link name="Logout" class="button is-light" to="/logout" v-show="loggedIn" @click="logout">Logout</router-link>
+              </div>
+            </div>
         </div>
         </nav>
     </div>
@@ -63,4 +67,8 @@
 </script>
 
 <style>
+.router-link-exact-active {
+  border-bottom: 2px solid;
+  border-radius: 2px;
+}
 </style>
