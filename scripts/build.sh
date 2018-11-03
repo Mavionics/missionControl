368e08aef1745c5c57930bbb2c8c2b1453780113
Alexander Poole <alex.o.poole@gmail.com>
2018-11-03 08:45:38 +0100

    Myby it dosn't run for windows resons

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
-START_TIME=$SECONDS
-
+echo "Pre branch"
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+echo $branch
 
 # Only push this update to the server if the current branch is the Master branch
-if [ "$branch" == "master" ]
-then
+if [ "$branch" == "master" ]; then
+
   echo "Compiling and deploying $branch to production..."
 
   npm run build
   firebase deploy -P default --token $FIREBASE_TOKEN --only hosting
-
-  ELAPSED_TIME=$(($SECONDS - $START_TIME))
-  echo "\nFinished in $ELAPSED_TIME seconds.\n\n"
-if [ "$branch" == "develop" ]
-then
+elif [ "$branch" == "develop" ]; then
   echo "Compiling and deploying $branch to development..."
 
   npm run dev-build
   firebase deploy -P develop --token $FIREBASE_TOKEN --only hosting
 
-  ELAPSED_TIME=$(($SECONDS - $START_TIME))
-  echo "\nFinished in $ELAPSED_TIME seconds.\n\n"
 else
     echo "Will not publish from a branch other than master or develop.\nPlease merge your changes into master and try again.\n\n"
 fi
