c944138351c0dcc562ee5b8d849f553d12506855
Alexander Poole <alex.o.poole@gmail.com>
2018-12-17 07:00:14 +0100

    Rename folder
    Renamed app to mavionics

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -4,16 +4,19 @@ echo "Extracting files..."
 
 tar xvf buildFiles.tar -C .
 
+cp keystore.jks app/android/app/keystore.jks
+cp keystore.properties app/android/app/keystore.properties
+
 # Only push this update to the server if the current branch is the Master branch
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo "Using google-service.json for build..."
 
-  cp google-services.json vueNautics/android/app/google-services.json
+  cp google-services.json app/android/app/google-services.json
 
 else
 
   echo "Using google-service.json-dev for build..."
 
-  cp google-services.json-dev vueNautics/android/app/google-services.json
+  cp google-services.json-dev app/android/app/google-services.json
 fi
