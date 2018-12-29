b5f0cb4f10233ae8ecc6616bb2d68a03a1bb6152
Henrik Wolkesson <henrik.wolkesson@gmail.com>
2018-12-29 13:14:08 +0100

    Added panels in cockpit

--- mavionics/src/views/Cockpit.vue
+++ mavionics/src/views/Cockpit.vue
@@ -1,16 +1,88 @@
 <template>
-  <div class="cockpit">   
-     <Layout>
-      <div class="container">
-        <h1>This is where you'll fly! VehicleId {{avId}} {{vehicle ? vehicle.name: ''}}</h1>
-        <div>All data: {{vehicle}}</div>
-      </div>
-    </Layout>
+  <div class="cockpit">
+    <div id="top" class="top-panel parent">
+      <router-link
+        name="ControlRoom"
+        class="navbar-item"
+        to="/controlroom"
+      >Home</font-awesome-icon></router-link>
+    </div>
+    <div id="video" class="fullscreen-panel parent">
+      <div class="middle">Video</div>
+      <video id="friendsVideo" autoplay playsinline></video>
+    </div>
+    <div id="map" class="secondary-panel parent">
+      <div class="middle">Map</div>
+    </div>
+    <div class="lower-panel parent" id="debug">
+      <div class="middle">Debug</div>
+      <video id="yourVideo" autoplay muted playsinline></video>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.parent {
+  position: absolute;
+  display: block;
+}
+
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #ffffff;
+  font-weight: bold;
+  font-size: larger;
+}
+
+.top-panel {
+  z-index: 3;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  color: darkslategrey;
+}
+
+.fullscreen-panel {
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  padding: 0;
+  margin: 0;
+  background: #009955;
+  text-align: center;
+}
+
+.secondary-panel {
+  left: 0;
+  bottom: 0;
+  height: 35vh;
+  width: 35vh; /* 16:9 ratio */
+  background: #005599;
+  margin: 8px;
+}
+
+.lower-panel {
+  left: 35vh;
+  margin: 8px;
+  bottom: 0;
+  height: 35vh;
+  background: rgba(0, 0, 0, 0.50);
+  right: 35vh;
+}
+
+.parent > video {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
+
 <script>
 import Layout from "@/components/Layout.vue";
+
 
 export default {
   name: "cockpit",
@@ -18,15 +90,23 @@ export default {
     Layout
   },
   mounted() {
+const yourVideo = document.getElementById("yourVideo");
+const friendsVideo = document.getElementById("friendsVideo");
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: true })
+      // .then(stream => alert(stream))
+      .then(stream => (yourVideo.srcObject = stream));
+    
     // Get data from db
-    this.$store.dispatch("connectToVehicle", {avId: this.avId});
-  },   
+    this.$store.dispatch("connectToVehicle", { avId: this.avId });
+  },
   computed: {
     vehicle() {
       return this.$store.state.currentVehicle;
     }
   },
-  props:{
+  props: {
     avId: String
   }
 };
