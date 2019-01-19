7eaf7ebc2f3cb09f309cc86d487adea2f54b22cc
Alexander Poole <alex.o.poole@gmail.com>
2019-01-19 18:34:19 +0100

    Show active vehicle name in flight screen

--- app/src/screen/flight.vue
+++ app/src/screen/flight.vue
@@ -6,16 +6,23 @@
             </nb-body>
             <nb-right />
         </nb-header>
+        <view class="text-container">
+        <text>{{activeVehicle.name}}</text>
+    </view>
 </template>
 
 <script>
 import React from 'react';
 import { Dimensions } from 'react-native';
+import store from '../store';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default {
   computed: {
+    activeVehicle () {
+      return store.state.activeVehicle;
+    }
   },
   props: {
       navigation: Object
