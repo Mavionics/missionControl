00a1ecc946c8add83b26a4b02df3b5f4a8d79cce
Wolkesson <henrik.wolkesson@gmail.com>
2019-02-02 12:34:06 +0100

    Remove non working new tests.

---
+++ mavionics/tests/unit/Store.login.spec.js
@@ -0,0 +1,19 @@
+// import { actions } from "@/store/actions.js";
+
+// import { expect } from "chai";
+
+// describe("actions", () => {
+//   it("Should set isAuthorized after login", () => {
+//     console.warn("In test!");
+//     const state = { isLoggedIn: false };
+//     const dispatch = function() {};
+//     const getters = {
+//       getUid() {
+//         return 0;
+//       }
+//     };
+//     //     actions.login({ state, dispatch, getters });
+
+//     expect(state.isLoggedIn).to.false;
+//   });
+// });
